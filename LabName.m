14b2f5ce5537b182ef990e770f29aed3afaa8dec
erica.nordin <erica.nordin@eustlab1-psy17.uleth.ca>
2017-07-05 15:16:09 -0600

    Created shell for classes GUI and Recording and created enumerations NamingMethod and LabName

---
+++ LabName.m
@@ -0,0 +1,15 @@
+classdef LabName
+    %UNTITLED2 Summary of this class goes here
+    %   Detailed explanation goes here
+    enumeration
+        Euston, Gibb, Metz
+    end
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
---
+++ NamingMethod.m
@@ -0,0 +1,15 @@
+classdef NamingMethod
+    %UNTITLED Summary of this class goes here
+    %   Detailed explanation goes here
+    enumeration
+        Auto, Manual
+    end
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
