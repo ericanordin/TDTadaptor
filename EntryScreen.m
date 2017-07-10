907168ef63d2f50f51b458c1bc9f5c287e5302e8
Erica Nordin <erica.nordin@uleth.ca>
2017-07-10 15:17:37 -0600

    Linked EntryScreen buttons to empty functions

--- EntryScreen.m
+++ EntryScreen.m
@@ -16,14 +16,28 @@ classdef EntryScreen < GUI
                 [10 450 400 30], 'String', ...
                 'Would you like to name your file yourself, or let me do it for you?');
             autoButton = uicontrol('Style', 'pushbutton', 'Position', ...
-                [10 200 200 150], 'String', 'Name Automatically (a)');
+                [10 200 200 150], 'String', 'Name Automatically (a)', ...
+                'Callback', @AutoName);
             manualButton = uicontrol('Style', 'pushbutton', 'Position', ...
-                [260 200 200 150], 'String', 'Name Manually (m)');
+                [260 200 200 150], 'String', 'Name Manually (m)', ...
+                'Callback', @ManualName);
+            %set(autoButton, 'callback', {@AutoName, this});
+            %set(manualButton, 'callback', @ManualName);
+            
+            function this = AutoName(src, ~)
+                'In AutoName'
+            end
+            function ManualName(src, ~)
+                'In ManualName'
+            end
         end
         
         function fig = display(guiobj)
         
         end
+        
+        
+        
     end
     
 end
