531ab91c973c244aea80d0cbd9818b81b327e540
Erica Nordin <erica.nordin@uleth.ca>
2017-07-10 16:10:30 -0600

    Built GUI for LabScreen

--- LabScreen.m
+++ LabScreen.m
@@ -2,13 +2,64 @@ classdef LabScreen < GUI
     %LABSCREEN Chooses the LabName.
     
     properties
+        guiF;
+        instructions;
+        eustonButton;
+        gibbButton;
+        metzButton;
     end
     
     methods
-        function guiobj = LabScreen()
+        function this = LabScreen()
+            guiF = figure('Name', 'Select Lab', 'NumberTitle', 'off',...
+                'Position', [100 100 500 500], 'WindowKeyPressFcn', ...
+                @Shortcuts);
+            instructions = uicontrol('Style', 'text', 'Position', ...
+                [10 450 400 30], 'String', ...
+                'Which lab is running this experiment?');
+            eustonButton = uicontrol('Style', 'pushbutton', 'Position', ...
+                [20 350 400 50], 'String', 'Euston (e)', ...
+                'Callback', {@Selection, 'e'});
+            gibbButton = uicontrol('Style', 'pushbutton', 'Position', ...
+                [20 250 400 50], 'String', 'Gibb (g)', ...
+                'Callback', {@Selection, 'g'});
+            metzButton = uicontrol('Style', 'pushbutton', 'Position', ...
+                [20 150 400 50], 'String', 'Metz (m)', ...
+                'Callback', {@Selection, 'm'});
+            
+                
+            function Shortcuts(src, eventdata)
+                Selection(src, eventdata, eventdata.Key);
+                %{
+                switch eventdata.Key
+                    case {'e'}
+                        
+                    case {'g'}
+                        
+                    case {'m'}
+                        
+                end
+                %}
+            end
+            
+            function chosenLab = Selection(src, eventdata, choice)
+                import Enums.LabName;
+                switch choice
+                    case {'e'}
+                        chosenLab = LabName.Euston;
+                        %'Chose Euston'
+                    case {'g'}
+                        chosenLab = LabName.Gibb;
+                    case {'m'}
+                        chosenLab = LabName.Metz;
+                end
+                chosenLab
+            end
+            
         end
         
-        display(guiobj)
+        function fig = display(guiobj)
+        end
     end
     
 end
