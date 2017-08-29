777ec1111a90f82b50c2ec97d0e17a5e13939fd2
Erica Nordin <erica.nordin@uleth.ca>
2017-08-29 16:50:53 -0600

    Fixed algorithm problem

--- WebcamBuffers.m
+++ WebcamBuffers.m
@@ -16,7 +16,7 @@ builtBuffer = zeros(1, npts*5); %5s buffer
 % serial buffer will be divided into two buffers A & B (to prevent the risk
 % of data in the buffer being overwritten)
 bufpts = npts/2;
-t=(1:bufpts)/npts;
+%t=(1:bufpts)/npts;
 
 rec1 = audiorecorder(npts, 16, 1);
 rec2 = audiorecorder(npts, 16, 1);
@@ -38,29 +38,38 @@ for i = 0:4
     while(curindex < bufpts)
         curindex = rec1.CurrentSample;
     end
-    stop(rec1);
-    disp('Stopping 1');
+
     
-    
-    record(rec2);
+    record(rec2); %Start new recording as quickly as possible to minimize
+    %intermittent data loss.
     disp('Recording 2');
     
+    stop(rec1);
+    curindex = 0;
+    disp('Stopped 1');
+    disp('rec1 samples: '); disp(rec1.TotalSamples);
+    %rec1 captures extra samples before it has time to stop
+    %Won't be a problem using TDT buffer
+    
+    
     samples1 = getaudiodata(rec1)';
-    builtBuffer(1, (1 + i*npts):(npts/2 + npts*i)) = samples1;
+    builtBuffer(1, (1 + i*npts):(npts/2 + npts*i)) = samples1(1:npts/2);
     %PlotLoop(samples1);
     play(rec1);
     disp('Copying 1');
     
-    while(curindex > bufpts)
+    while(curindex < bufpts)
         curindex = rec2.CurrentSample;
     end
 
     stop(rec2);
-    disp('Stopping 2');
-    
+    disp('Stopped 2');
+    disp('rec2 samples: '); disp(rec2.TotalSamples);
+    curindex = 0;
     samples2 = getaudiodata(rec2)';
-    builtBuffer(1, (1 + i*npts + npts/2):(npts*(i+1))) = samples2;
+    builtBuffer(1, (1 + i*npts + npts/2):(npts*(i+1))) = samples2(1:npts/2);
     %PlotLoop(samples2);
+    
     play(rec2);
     disp('Copying 2');
     
