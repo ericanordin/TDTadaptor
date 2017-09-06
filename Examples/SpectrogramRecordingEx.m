9c03c5bd4f03738d3f97dc2eb782220d53760081
Erica Nordin <erica.nordin@uleth.ca>
2017-09-06 12:47:16 -0600

    Got resetting buffers working.

--- Examples/SpectrogramRecordingEx.m
+++ Examples/SpectrogramRecordingEx.m
@@ -1,14 +1,14 @@
 rec = audiorecorder(48000, 16, 1);
-recordblocking(rec, 0.5); %Records for 5 seconds
+recordblocking(rec, 2); %Records for 5 seconds
 %or record(rec); stop(rec);
 %record(rec);
 samples = getaudiodata(rec);
 disp(samples);
 numSamples = rec.TotalSamples;
-spectrogram(samples, 256, 4, [], numSamples, 'yaxis');
-%figure(1);
+[~, f, t, p] = spectrogram(samples, 256, 4, [], numSamples, 'yaxis');
+figure(1);
 %[~, f, t, p] = spectrogram(samples, 256, 4, [], numSamples, 'yaxis');
-%specPlot = imagesc(t, f, 10*log10(p+eps));
+specPlot = imagesc(t, f, 10*log10(p+eps));
 %specPlot.PropertyList;
 %buff = specPlot.YData;
 colormap('gray');
--- Graph Experiments/WebcamBuffersRegReset.m
+++ Graph Experiments/WebcamBuffersRegReset.m
@@ -1,7 +1,7 @@
 function WebcamBuffersRegReset()%screen)
 % For attempting the version in which the graphs reset with a new
 % buffer after a specified amount of repetitions.
-% Only plotting last section of buffer
+% Pause is necessary for plot to appear on each iteration.
 
 %import GUIFiles.RecordScreen
 
@@ -11,14 +11,13 @@ delete(findall(0, 'Type', 'figure'));
 
 % size of the entire serial buffer
 npts = 200000;%48000; %sampling frequency
-buffLength = 2; %5s buffer
-totalLength = 6; %30s recording
+buffLength = 2; %2s buffer
+totalLength = 6; %6s recording
 builtBuffer = zeros(1, npts*buffLength);
 
 % serial buffer will be divided into two buffers A & B (to prevent the risk
 % of data in the buffer being overwritten)
 bufpts = npts/2;
-%t=(1:bufpts)/npts;
 
 rec1 = audiorecorder(npts, 16, 1);
 rec2 = audiorecorder(npts, 16, 1);
@@ -28,20 +27,15 @@ rec2 = audiorecorder(npts, 16, 1);
 
 % begin acquiring
 curindex = rec1.CurrentSample;
-disp(['Current buffer index: ' num2str(curindex)]);
 
-%record(rec1);
 waveFig = figure(1);
-%clf;
 set(waveFig, 'Name', 'Waveform');
-%set(waveFig, 'Name', 'Spectrogram (Straight Plotted)');
-%{
+
 specFig = figure(2);
-clf;
 set(specFig, 'Name', 'Spectrogram (Modified)');
-%}
+
 wavePlot = '';
-%specPlot = '';
+specPlot = '';
 %isobject returns false
 
 x = 1:npts*buffLength;
@@ -53,7 +47,6 @@ for totalReps = 0:(totalLength/buffLength-1)
     for chunkSecond = 0:(buffLength-1)
         
         record(rec1);
-        %disp('Recording 1');
         
         while(curindex < bufpts)
             curindex = rec1.CurrentSample;
@@ -62,104 +55,75 @@ for totalReps = 0:(totalLength/buffLength-1)
         
         record(rec2); %Start new recording as quickly as possible to minimize
         %intermittent data loss.
-        %disp('Recording 2');
         
         stop(rec1);
         
         curindex = 0;
-        %disp('Stopped 1');
-        %disp('rec1 samples: '); disp(rec1.TotalSamples);
-        %rec1 captures extra samples before it has time to stop
+
+        %record captures extra samples before it has time to stop
         %Won't be a problem using TDT buffer
-        
-        
         samples1 = getaudiodata(rec1)';
         builtBuffer(1, (1 + chunkSecond*npts):(npts/2 + npts*chunkSecond)) = samples1(1:npts/2);
-        %PlotLoop(samples1);
-        %play(rec1);
-        %disp('Copying 1');
         
         while(curindex < bufpts)
             curindex = rec2.CurrentSample;
         end
         
         stop(rec2);
-        %disp('Stopped 2');
-        %disp('rec2 samples: '); disp(rec2.TotalSamples);
+        
         curindex = 0;
         samples2 = getaudiodata(rec2)';
-        builtBuffer(1, (1 + chunkSecond*npts + npts/2):(npts*(chunkSecond+1))) = samples2(1:npts/2);
-        %PlotLoop(samples2);
-        
-        %play(rec2);
-        %disp('Copying 2');
-        
+        builtBuffer(1, (1 + chunkSecond*npts + npts/2):(npts*(chunkSecond+1))) = samples2(1:npts/2); 
     end
-
+    
     
     if ~isobject(wavePlot)
-        disp('In first rep "if"');
-        
-        %figure(1);
-        %hold on;
-        %firstSamples = builtBuffer;
+        figure(1);
         wavePlot = plot(x,builtBuffer);
         
         title('Waveform');
-        xlabel('Data Point');
+        xlabel('Seconds');
         ylabel('Intensity');
-
-        %hold off;
+        
+        figure(2);
+        [~, f, t, p] = spectrogram(builtBuffer, 1024, 256, [], npts, 'yaxis');
+        specPlot = imagesc(t, f, 10*log10(p+eps));%log10(abs(s)));
+        
+        title('Spectrogram');
+        xlabel('Seconds');
+        ylabel('Frequency (kHz)');
+        specDB = colorbar;
+        ylabel(specDB, 'dB');
+        colormap('gray');
         
     else
-        disp('In non-first "else"');
         x = x +(npts*buffLength);
-        %figure(1);
-        %hold on;
+        
+        figure(1);
         wavePlot.YData = builtBuffer;
         wavePlot.XData = x;
         
-        %hold off;
+        figure(2);
+        [~, ~, ~, p] = spectrogram(builtBuffer, 1024, 256, [], npts, 'yaxis');
+        specPlot.CData = 10*log10(p+eps);
+        specPlot.XData = specPlot.XData + buffLength;
     end
-    %{
+    
+    
     figure(1);
-    hold on;
     waveAxes = waveFig.CurrentAxes;
     xScale = get(waveAxes, 'XTick');
     xScale = xScale./npts;
     set(waveAxes,'XTickLabel', xScale);
-    hold off;
-    %}
     
-    %{
-
-    %Somewhat normalized:
-    %dB are represented somewhat higher than they are originally.
-    [~, f, t, p] = spectrogram(builtBuffer, 1024, 256, [], npts, 'yaxis');
-    %figure(1);
-    %hold on;
-    %axis([0 buffLength 0 80000]);
-    %spectrogram(builtBuffer, 1024, 256, [], npts, 'yaxis');
-    
-    %hold off;
     figure(2);
-    clf;
-    hold on;
-    specPlot = imagesc(t, f, 10*log10(p+eps));%log10(abs(s)));
-    axis([0 buffLength 0 80000]);
+    axis([totalReps*buffLength (totalReps+1)*buffLength 0 80000]);
     specAxes = specFig.CurrentAxes;
     yScale = get(specAxes, 'YTick');
     yScale = yScale./1000;
     set(specAxes, 'Ydir', 'Normal', 'YTickLabel', yScale);
     
-    title('Spectrogram');
-    xlabel('Seconds');
-    ylabel('Frequency (kHz)');
-    specDB = colorbar;
-    ylabel(specDB, 'dB');
-    colormap('gray');
-    hold off;
-    %}
+    pause(0.01);
 end
 toc;
 end
