887826304a7e46c9bd4a046e49783153cbbe717d
Erica Nordin <erica.nordin@uleth.ca>
2017-09-07 13:10:31 -0600

    Expanded checkOverwrite to check for other criteria

--- +StandardFunctions/checkOverwrite.m
+++ +StandardFunctions/checkOverwrite.m
@@ -2,19 +2,20 @@ function checkOverwrite(filePath, displayField, errorColor)
 %checkOverwrite checks whether a file with the same name and location
 %exists.
 %   The displayField is coloured red if recording will overwrite an
-%   existing file.
-%Also colours red if the file name does not end in .wav
+%   existing file, if the file name does not end in .wav, if it is not
+%   saving to a drive, or the file is unnamed after the directory.
 
-if exist(filePath, 'file') == 2
-    %Prevent overwrite
+fileDesignation = filePath(end-3:end);
+directoryPrefix = filePath(2:3);
+preWav = filePath(end-4);
+validName = strcmp(fileDesignation, '.wav') && strcmp(directoryPrefix, ':\') ...
+    && ~strcmp(preWav, '\');
+
+if exist(filePath, 'file') == 2 || ~validName
+    %Prevent overwrite or invalid path
     set(displayField, 'BackgroundColor', errorColor);
 else
-    fileDesignation = filePath(end-3:end);
-    if fileDesignation ~= '.wav'
-        set(displayField, 'BackgroundColor', errorColor);
-    else
-        set(displayField, 'BackgroundColor', 'white');
-    end
+    set(displayField, 'BackgroundColor', 'white');
 end
 
 end
