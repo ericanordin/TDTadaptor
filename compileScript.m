41696dc8163b70521222342e0ed555ac108e56c7
Erica Nordin <erica.nordin@uleth.ca>
2017-11-18 10:22:43 -0700

    New test executables

--- compileScript.m
+++ compileScript.m
@@ -1,5 +1,11 @@
 %Ultrasonic_Recording_Interface
+%Directly on computer:
 mcc -e -d 'C:\Ultrasonic Recording Program' -o Ultrasonic_Recording_Interface main.m
+%At home:
+mcc -e -d 'G:\Fall 2017 Co-op\TDTadaptor\Executables and Key Files\Ultrasonic_Recording_Interface' -o Ultrasonic_Recording_Interface main.m
 
 %BinaryToWav
+%Directly on computer:
 mcc -e -d 'C:\Ultrasonic Recording Program' -o BinaryToWav BinaryToWav.m
+%At home:
+mcc -e -d 'G:\Fall 2017 Co-op\TDTadaptor\Executables and Key Files\BinaryToWav' -o BinaryToWav BinaryToWav.m
