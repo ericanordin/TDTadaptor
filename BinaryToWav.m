7b771704c43d6a83eb2494468702292d13ac939b
Erica Nordin <erica.nordin@uleth.ca>
2017-11-18 17:44:50 -0700

    Adjusted BinaryToWav for 16 or 32 bit

--- BinaryToWav.m
+++ BinaryToWav.m
@@ -2,17 +2,33 @@ function BinaryToWav()
 %BinaryToWave converts .f32/.i16 files to .wav in the case of program crash before
 %the conversion has occurred in AcquireAudio.
 
-[fileName, filePath, ~] = uigetfile({'*.i16', '*.f32'}, 'Select binary file');
+[fileName, filePath, ~] = uigetfile({'*.i16;*.f32', ...
+    'Binary Files(*.i16, *.f32)'}, 'Select binary file');
 
 fullPath = strcat(filePath, fileName);
 [~, ~, ext] = fileparts(fullPath);
-info = audioinfo(fullPath);
+disp(ext);
+if strcmp(ext, '.I16')
+    bits = 16;
+    precision = '*int16';
+else
+    if strcmp(ext, 'F32')
+        bits = 32;
+        precision = '*float32';
+    else
+        %disp('Compare not working');
+        msgbox('Error reading file. Conversion not completed.');
+        quit;
+    end
+end
 
 Wavfile = fullPath(1:end-3);
 Wavfile = strcat(Wavfile, 'wav');
+disp(Wavfile);
 binaryFile = fopen(fullPath, 'r');
-totalSound = fread(binaryFile, ['*' ext]);
-audiowrite(Wavfile, totalSound, info.sampleRate, 'BitsPerSample', info.BitsPerSample);
+totalSound = fread(binaryFile, precision);
+audiowrite(Wavfile, totalSound, 195312, 'BitsPerSample', bits);
+%195312 is the frequency for the TDT
 
 fclose(binaryFile);
 delete(fullPath); %Deletes binary file
