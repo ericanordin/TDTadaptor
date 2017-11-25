a8c4d664d6447332963eb0031cf66f0bdb1f515e
Erica Nordin <erica.nordin@uleth.ca>
2017-11-25 15:00:03 -0700

    conversion program change

--- convertWav32to24Bit.m
+++ convertWav32to24Bit.m
@@ -1,4 +1,7 @@
 %32-bit float WAVE files are converted into 24-bit float WAVE files
+%WARNING: This is a temporary script that scales the audio data down by
+%10x. Do not use this script after the Ultrasonic Recording program has
+%been updated to integrate scaling.
 
 [fileName, filePath, ~] = uigetfile('*.wav', ...
     'Select 32-bit WAVE file');
