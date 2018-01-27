5398b652e40aad2dfd7765a1c2eb77a2d33de3f0
Erica Nordin <erica.nordin@uleth.ca>
2018-01-27 13:32:22 -0700

    Compiled Downsampler and tested

--- compileScript.m
+++ compileScript.m
@@ -10,6 +10,10 @@ mcc -e -d 'C:\Ultrasonic Recording Program' -o BinaryToWav BinaryToWav.m
 %At home:
 mcc -e -d 'G:\Fall 2017 Co-op\TDTadaptor\Executables and Key Files\BinaryToWav' -o BinaryToWav BinaryToWav.m
 
+%Downsampler
+%At home: 
+mcc -e -d 'G:\Fall 2017 Co-op\TDTadaptor\Executables and Key Files\Downsampler' -o DownsamplerForWAVs Downsampler.m
+
 %convertWav32to24Bit:
 %At home:
 mcc -e -d 'G:\Fall 2017 Co-op\TDTadaptor\Executables and Key Files\32 to 24 Bit Converter' -o Converter_32_to_24_Bit convertWav32to24Bit.m
