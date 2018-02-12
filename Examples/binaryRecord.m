43d92e8517caf6f35cd3965306e8ed2fedc69efc
Erica Nordin <erica.nordin@uleth.ca>
2018-02-12 13:55:58 -0700

    Removed scrap files

--- Examples/binaryRecord.m
+++
@@ -1,7 +0,0 @@
-fnoise = fopen('G:\TDT\test.I16', 'w');
-recorder = audiorecorder(195312, 16, 1, 1);
-record(recorder, 3);
-pause(3);
-samples = getaudiodata(recorder);
-fwrite(fnoise, samples, 'int16');
-fclose(fnoise);
